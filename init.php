99e9739f50fbefb1c3b6176b2b7114bfd4be4ac9
mighdad <mr.mighdadjaba@gmail.com>
2025-06-21 07:10:41 +0800

    hapus cloud

---
+++ init.php
@@ -0,0 +1,6 @@
+<?php
+// Direktori vendor telah dihapus, jadi file ini tidak lagi memuat dependensi.
+// Dapat digunakan untuk konfigurasi atau fungsi global lainnya di masa depan.
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
